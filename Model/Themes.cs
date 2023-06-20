ab98ef78c04f0c8f9342d047d27e676e25d6f9b0
vanka334 <isip_i.d.kuzin@mpt.ru>
2023-06-21 00:31:19 +0300

    Сделал библиотеку(без фона, его надо отдельно вместе с логикой для смены тем)(шрифты кое-где не работают)

---
+++ Model/Themes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunClounds.Model
+{
+    internal class Themes
+    {
+       public static async Task Changer()
+        {
+            await Task.Run(() =>
+            {
+                DateTime dateTime = DateTime.Now;
+            });
+        }
+    }
+}
+///ЭЭЭ НЕ ТРОГАЙ ДА???? ВАНЯ ДОПИШЕТ!!
